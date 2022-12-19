a76e1bb4fd79304bb3ddd990d7593f89ed005f10
frank <franksavena@gmail.com>
2022-12-19 17:50:51 -0600

    commit tree edbefcb4e0b90e10e3cd329d7296b6d418ffd343

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+require 'pry'
+katz_deli= []
+
+def line (katz_deli)
+    if katz_deli == []
+       puts "The line is currently empty."
+    else
+        b = 0
+       message ="The line is currently:" 
+        katz_deli.each do |a| 
+        b += 1 
+      message = message + " #{b}. #{a }"
+        
+        end
+       puts message
+    end
+end
+
+
+
+# line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name)
+   puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
+
+end
+
+def now_serving(katz_deli)
+    if katz_deli == []
+       puts "There is nobody waiting to be served!"
+    else
+      puts  "Currently serving #{katz_deli[0]}."
+        katz_deli.shift
+    end
+
+end
